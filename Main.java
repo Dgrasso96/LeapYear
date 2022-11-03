cc9ce2fcee42d36a9a280bbbed695a151370dc02
DavideG96 <davidegrasso966@gmail.com>
2022-11-03 16:26:58 +0100

    start project

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int val = 0;
+
+		System.out.println("Inserisci l'anno:");
+		val = in.nextInt();
+
+		if (Bisestile.LeapYear(val)) {
+			System.out.println(val + " è un anno bisestile");
+		} else {
+			System.out.println(val + " non è un anno bisestile");
+		}
+	}
+}
+
+public class Bisestile {
+	public static boolean LeapYear(int year) {
+
+		if (year > 1 && year < 9999) {
+			if (((year % 4) == 0 && (year % 100) != 0) || ((year % 400) == 0)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
